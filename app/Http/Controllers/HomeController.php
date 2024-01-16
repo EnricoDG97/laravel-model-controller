3d5e699714f877999c590b4e8d8a2e019c35c481
EnricoDG97 <enrico.delgaudio97@gmail.com>
2024-01-16 10:25:53 +0100

    feat: created controller and model, linked to movies_db

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $movies = Movie::all();
+        // $movies = Movie::where('id', 4)->first();
+        dd($movies);
+        return view('home', compact('movies'));
+    }
+}
